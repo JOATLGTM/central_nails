7706818f04b0a26d5eff0f5e4970618233489304
JOAT <joat@JOATs-iMac-Pro.local>
2020-06-17 14:22:14 -0400

    created home component. Inserted Routing. Added footer and main text placeholder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import React from 'react';
+import { Switch, Link, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Services from './components/Services'
+import Gallery from './components/Gallery'
+import Aesthetics from './components/Aesthetics'
+import Contact from './components/Contact'
 import './App.css';
 
 function App() {
   return (
-    <div class="app">
-      <header class="header"></header>
-      <main class="main">
-        <div class="container">
-          <div class="information">
-          </div>
-          <div class="information">
-          </div>
+    <div className="app">
+      <header className="header">
+        <div>
+          <div><p style={{textAlign: "center"}}>This is where the logo would go</p></div>
         </div>
+        <div className="links">
+          <div className="link"><Link to="/">Home</Link></div>
+          <div className="link"><Link to="/services">Services</Link></div>
+          <div className="link"><Link to="/gallery">Gallery</Link></div>
+          <div className="link"><Link to="/aesthetics">Aesthetics</Link></div>
+          <div className="link"><Link to="/contact">Contact</Link></div>
+        </div>
+      </header>
+      <main className="main">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/aesthetics" component={Aesthetics} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
       </main>
-      <footer class="footer"></footer>
+      <footer className="footer">
+        <div>
+          <h4>Hours</h4>
+          <p>
+            Mon - Fri 10am-7:30pm
+            <br/>
+            Sat 10am-6:30
+            <br/>
+            Sun Closed
+          </p>
+        </div>
+        <div>
+          <h4>Contact</h4>
+          <p>
+            614-309-3089
+          </p>
+        </div>
+        <div>
+          <h4>Location</h4>
+          <p>
+          1357 Worthington Centre Dr 
+          <br/>
+          Worthington, OH 43085
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+            <div className="container item-1">
+                <div className="information">
+                    <div className="temp-img"></div>
+                </div>
+                <div className="information">
+                    <div className="temp-text">
+                    <p>Lorem Khaled Ipsum is a major key to success. To succeed you must believe. When you believe, 
+                        you will succeed. The other day the grass was brown, now it’s green because I ain’t give up. 
+                        Never surrender. A major key, never panic. Don’t panic, when it gets crazy and rough, don’t panic, 
+                        stay calm. Life is what you make it, so let’s make it. Mogul talk. Egg whites, turkey sausage, 
+                        wheat toast, water. Of course they don’t want us to eat our breakfast, so we are going to enjoy our breakfast. 
+                        The key is to enjoy life, because they don’t want you to enjoy life. I promise you, they don’t want you to jetski, 
+                        they don’t want you to smile.</p>
+                    </div>
+                </div>
+                </div>
+                <div className="container item-2">
+                <div className="information">
+                    <div className="temp-text">
+                    <p>Find peace, life is like a water fall, you’ve gotta flow. Celebrate success right, the only way, apple. 
+                        They key is to have every key, the key to open every door. Mogul talk. Find peace, life is like a water fall, 
+                        you’ve gotta flow. Bless up. Lion! The key to more success is to have a lot of pillows. Always remember in the 
+                        jungle there’s a lot of they in there, after you overcome they, you will make it to paradise. In life there will be 
+                        road blocks but we will over come it. Lion!</p>
+                    </div>
+                </div>
+                <div className="information">
+                    <div className="information">
+                    <div className="temp-img"></div>
+                    </div>
+                </div>
+                </div>
+                <div className="container item-1">
+                <div className="information">
+                    <div className="temp-img"></div>
+                </div>
+                <div className="information">
+                    <div className="temp-text">
+                    <p>Lorem Khaled Ipsum is a major key to success. To succeed you must believe. When you believe, 
+                        you will succeed. The other day the grass was brown, now it’s green because I ain’t give up. 
+                        Never surrender. A major key, never panic. Don’t panic, when it gets crazy and rough, don’t panic, 
+                        stay calm. Life is what you make it, so let’s make it. Mogul talk. Egg whites, turkey sausage, 
+                        wheat toast, water. Of course they don’t want us to eat our breakfast, so we are going to enjoy our breakfast. 
+                        The key is to enjoy life, because they don’t want you to enjoy life. I promise you, they don’t want you to jetski, 
+                        they don’t want you to smile.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
